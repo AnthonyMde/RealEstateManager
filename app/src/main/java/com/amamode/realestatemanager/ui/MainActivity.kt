ae4275b6d94ffbdaed49dd4a1ba0c50135f56b25
Anthony Mamode <amamode@appstud.com>
2020-04-26 22:26:44 +0200

    Add navigation to creation fragment for both tablet and mobile
    Add listener on navigation to hide/show activity toolbar dynamically.

--- app/src/main/java/com/amamode/realestatemanager/ui/MainActivity.kt
+++ app/src/main/java/com/amamode/realestatemanager/ui/MainActivity.kt
@@ -4,15 +4,40 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import com.amamode.realestatemanager.R
 import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var controller: NavController
     private val isTablet by lazy { resources.getBoolean(R.bool.isTablet) }
+    private val listener = NavController.OnDestinationChangedListener { _, navDestination, _ ->
+        if (navDestination.label in listOf("EstateCreationFragment")) {
+            supportActionBar?.hide()
+        } else {
+            supportActionBar?.show()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        controller = findNavController(R.id.main_nav_container)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        controller.addOnDestinationChangedListener(listener)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        controller.removeOnDestinationChangedListener(listener)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -24,7 +49,7 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.add_estate -> {
-                toast("Création ")
+                controller.navigate(R.id.action_list_dest_to_estateCreationFragment)
             }
             R.id.edit_estate -> {
                 toast("Modify ")
--- app/src/main/java/com/amamode/realestatemanager/ui/home/EmptyDetailFragment.kt
+++ app/src/main/java/com/amamode/realestatemanager/ui/home/EmptyDetailFragment.kt
@@ -2,16 +2,17 @@ package com.amamode.realestatemanager.ui.home
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.amamode.realestatemanager.R
 import kotlinx.android.synthetic.main.fragment_empty_detail_fragment.*
-import org.jetbrains.anko.support.v4.toast
 
 class EmptyDetailFragment : Fragment(R.layout.fragment_empty_detail_fragment) {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
         emptyViewAddEstateCta.setOnClickListener {
-            toast("Création")
+            activity?.findNavController(R.id.main_nav_container)
+                ?.navigate(R.id.action_list_dest_to_estateCreationFragment)
         }
     }
 }
