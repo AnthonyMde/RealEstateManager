956ade376e015da6c7269ed468f40c3f90dd6050
Anthony Mamode <amamode@appstud.com>
2020-07-30 15:03:28 +0200

    [wip] saving photo
    Only save photo when took it directly from device

--- app/src/main/java/com/amamode/realestatemanager/ui/creation/EstateCreationPhotoStepFragment.kt
+++ app/src/main/java/com/amamode/realestatemanager/ui/creation/EstateCreationPhotoStepFragment.kt
@@ -2,12 +2,15 @@ package com.amamode.realestatemanager.ui.creation
 
 import android.app.Activity.RESULT_OK
 import android.content.Intent
-import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.FileProvider
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
@@ -15,15 +18,19 @@ import com.amamode.realestatemanager.R
 import com.amamode.realestatemanager.domain.EstateDetails
 import com.amamode.realestatemanager.ui.EstateViewModel
 import kotlinx.android.synthetic.main.fragment_estate_creation_photo_step.*
+import org.jetbrains.anko.support.v4.toast
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
+import java.io.File
 
-private const val REQUEST_IMAGE_CAPTURE = 1
+
+private const val REQUEST_IMAGE_CAPTURE = 1034
 
 class EstateCreationPhotoStepFragment : Fragment(R.layout.fragment_estate_creation_photo_step) {
     private val estateViewModel: EstateViewModel by sharedViewModel()
     private val safeArgs: EstateCreationPhotoStepFragmentArgs by navArgs()
     private val estateToModify: EstateDetails? by lazy { safeArgs.estateToModify }
     private val isTablet: Boolean by lazy { resources.getBoolean(R.bool.isTablet) }
+    private var photoFile: File? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -45,18 +52,22 @@ class EstateCreationPhotoStepFragment : Fragment(R.layout.fragment_estate_creati
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            val imageBitmap = data?.extras?.get("data") as? Bitmap
+            /*val imageBitmap = data?.extras?.get("data") as? Bitmap
             if (imageBitmap != null) {
                 imageViewTest.setImageBitmap(imageBitmap)
             } else {
                 val uri = data?.data
                 imageViewTest.setImageURI(uri)
-            }
+            }*/
+            val takenImage = BitmapFactory.decodeFile(photoFile?.absolutePath)
+            imageViewTest.setImageBitmap(takenImage);
+        } else { // Result was a failure
+            toast("Picture wasn't taken!")
         }
     }
 
     private fun dispatchTakePictureIntent() {
-        val galleryIntent = Intent()
+        /*val galleryIntent = Intent()
         galleryIntent.type = "image/*"
         galleryIntent.action = Intent.ACTION_GET_CONTENT
 
@@ -67,8 +78,27 @@ class EstateCreationPhotoStepFragment : Fragment(R.layout.fragment_estate_creati
         chooser.putExtra(
             Intent.EXTRA_INITIAL_INTENTS,
             arrayOf(Intent(MediaStore.ACTION_IMAGE_CAPTURE))
+        )*/
+        */
+
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        photoFile = getPhotoFileUri()
+
+        val fileProvider: Uri =
+            FileProvider.getUriForFile(requireContext(), "com.realestate.fileprovider", photoFile!!)
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider)
+
+        if (intent.resolveActivity(activity!!.packageManager) != null) {
+            // Start the image capture intent to take photo
+            startActivityForResult(intent, REQUEST_IMAGE_CAPTURE)
+        }
+    }
+
+    private fun getPhotoFileUri(): File? {
+        return File(
+            context?.getExternalFilesDir(Environment.DIRECTORY_PICTURES),
+            "share_image_" + System.currentTimeMillis() + ".png"
         )
-        startActivityForResult(chooser, REQUEST_IMAGE_CAPTURE)
     }
 
     private fun setupToolbar() {
